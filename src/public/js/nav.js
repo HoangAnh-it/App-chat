dcf8e8e8c71d7f245af5c2b145c068be08f568d3
HoangAnh-it <vha31102002@gmail.com>
2022-04-18 17:33:13 +0700

    update header

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -1,6 +1,6 @@
 const btnChatBox = $('.nav .nav__chat');
 const btnAllUsers = $('.nav .nav__all-users');
-const btnFriends = $('.nav .nav__friends');
+const btnMail = $('.nav .nav__mail');
 
 btnChatBox.onclick = function () {
     window.location.href = '/api/v2/chat';
@@ -10,6 +10,6 @@ btnAllUsers.onclick = function () {
     window.location.href = '/api/v2/all-users?page=1'
 }
 
-btnFriends.onclick = function () {
+btnMail.onclick = function () {
     window.location.href = '#';
 }
